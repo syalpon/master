66d1810c31ac182e8852d90db7304e931164b019
syalpon <onoue0107@gmail.com>
2021-04-04 13:57:38 +0900

    テストウィンドウプログラム追加

--- C#/rpg_ver2.cs
+++ C#/rpg_ver2.cs
@@ -11,8 +11,9 @@ namespace main_frame{
 			Status p = new Status();
 			p.Show();
 			p.ex += 160;
-			p.hp -= 110;
+			p.hp += 110;
 			p.Show();
+			
 		}
 	}
 	
@@ -42,7 +43,7 @@ namespace main_frame{
 		
 		/* プロパティ */
 		public int max{
-			set{Max_Update(value);} 
+			set{Max_Update(value);}
 			get{return _max;}
 		}
 		
@@ -67,31 +68,37 @@ namespace main_frame{
 		}
 		/*------------- 以上コンストラクタ -------------*/
 		
+		/*------------- 以下演算子多重定義 -------------*/
+		public static object operator+ (Parameter x , int y) {x.now += y;return x;}
+		public static object operator- (Parameter x , int y) {x.now -= y;return x;}
+		public static object operator* (Parameter x , int y) {x.now *= y;return x;}
+		public static object operator/ (Parameter x , int y) {x.now /= y;return x;}
+		public static implicit operator object(int x) {return x;}
+		/*------------- 以上演算子多重定義 -------------*/
+		
 		/* 仮想メソッド */
 		protected virtual void Max_Update(int v){
 			/* 上限が下限を下回らないようにする */
 			if( _min <= v ){_max = v;}
 		}
 		protected virtual void Mid_Update(int v){
-			if( _min < v && v <= _max ){
-				_now = v;
-			}else if( v <= _min ){
-				Min_Over(v);
-			}else if( _max < v ){
-				Max_Over(v);
-			}else{
-				/* Do nothing */
-			}
+		
+			if			( _min < v && v <= _max )		{_now = v;}
+			else	if( v <= _min )								{Min_Over(v);}
+			else	if( _max < v )								{Max_Over(v);}
+			else																{/* Do nothing */}
 		}
 		protected virtual void Min_Update(int v){
 			/* 下限が上限を上回らないようにする */
 			if( v <= _max ){_min = v;}
 		}
+		
 		/* 現在値が上限/下限を超えた場合の処理：上限/下限でストップさせる */
 		protected virtual void Max_Over(int v){_now = _max;}
 		protected virtual void Min_Over(int v){_now = _min;}
+		
 		/* 表示 */
-		public virtual void Show(string name,string m,string n){System.Console.Write( "{0}[{1}] : {2}/{3}{4}",m,name,_now,_max,n);}
+		public virtual void Show(string name,string m,string n){System.Console.Write( "{0}[{1}] : {2}/{3}{4}",m, name,_now,_max,n);}
 	}
 	
 	
@@ -114,12 +121,6 @@ namespace main_frame{
 				_now = v;
 			}
 		}
-		
-		/* 演算子のオーバーライド(加減乗除) */
-		public static Hp operator+ (Hp x , int y) {x.now += y;return x;}
-		public static Hp operator- (Hp x , int y) {x.now -= y;return x;}
-		public static Hp operator* (Hp x , int y) {x.now *= y;return x;}
-		public static Hp operator/ (Hp x , int y) {x.now /= y;return x;}
 		
 		/* 表示メソッド */
 		public void Show(string m,string n){base.Show("HP",m,n);}
@@ -160,16 +161,13 @@ namespace main_frame{
 	/* 経験値クラス */
 	/*--------------*/
 	class Ex : Parameter{
-
-		public Lv lv;
-    
 		/* コンストラクタ */
 		public Ex():this(100,0,0){}
 		public Ex(int top):this(top,0,0){}
 		public Ex(int top,int bot):this(top,bot,bot){}
 		public Ex(int top,int mid,int bot):base(top,mid,bot){}
 		
-		/* 経験値が溜まったときの処理処理 */
+		/* 経験値が溜まったときの処理 */
 		protected override void Max_Over(int v){
 			now = v - max;	/*超過分は持ち越し*/
 		}
@@ -216,6 +214,18 @@ namespace main_frame{
 	}	
 	
 	
+	/*------------*/
+	/* 攻撃クラス */
+	/*------------*/
+	class Atk : Parameter {
+		public Atk():base(999,10,0){}
+		public void Damege(Character c){
+			System.Console.WriteLine( now+"のダメージ!");
+			c.hp -= now;
+		}
+	}
+	
+	
 	
 	/*------------------*/
 	/* ステータスクラス */
@@ -226,6 +236,7 @@ namespace main_frame{
 		public Mp mp;
 		public Ex ex;
 		public Lv lv;
+		public Atk atk;
 		
 		/* コンストラクタ */
 		public Status(){
@@ -234,8 +245,8 @@ namespace main_frame{
 			mp = new Mp();
 			lv = new Lv();
 			ex = new Ex();
+			atk= new Atk();
 		}
-		
 		
 		/* 表示 */
 		public void Show(){
@@ -248,31 +259,16 @@ namespace main_frame{
 			System.Console.Write("------------------------------------------\n\n");
 		}
 	}
+	
+	/*キャラクタークラス*/
+	class Character : Status {
+		/*コンストラクタ*/
+		public Character(){}
+		public Character(string n){name = n;}
+		
+		public static Character operator+ (Character x , Character y) { x.hp -= y.atk.now; return x;}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
