b79fd8d2d891eb3be9d62dc75f0ed837668093cf
hosomi.takuya <accelerator916@gmail.com>
2021-04-30 19:39:45 +0900

    プロジェクトをコミット

---
+++ C#/CreateFigureOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateFigureOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
