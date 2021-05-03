28e6bf3b96eddbc0f13f86a5f54a0a6d2d4b6db8
syalpon <onoue0107@gmail.com>
2021-05-03 19:00:25 +0900

    必要なもの以外削除

--- C#/rpg/ATC.cs
+++
@@ -1,9 +0,0 @@
-namespace RPG{
-	class ATC : Param {
-		/*コンストラクタ*/
-		public ATC(){}
-		public ATC(int x):base(x,x,0){}
-		public ATC(int x,int y):base(x,x,y){}
-		public ATC(int x,int y,int z):base(x,y,z){}
-	}
-}	
