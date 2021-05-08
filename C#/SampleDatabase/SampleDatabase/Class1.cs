970af13148c1ced10721f3b200f24379aaa63f32
hosomi.takuya <accelerator916@gmail.com>
2021-05-08 18:01:43 +0900

    データベース参考プロジェクト

---
+++ C#/SampleDatabase/SampleDatabase/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleDatabase.Model;
+
+namespace SampleDatabase
+{
+    public class Class1
+    {
+        static void InsertBooks()
+        {
+            using (var db = new BooksDbContext())
+            {
+                var book1 = new Book
+                {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+
+                db.Books.Add(book1);
+
+                var book2 = new Book
+                {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author
+                    {
+                        Birthday = new DateTime(1909, 6, 19),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+                db.Books.Add(book2);
+                db.SaveChanges();
+            }
+           
+        }
+
+        public static void Main(string[] args)
+        {
+            InsertBooks();
+        }
+    }
+}
---
+++ C#/SampleDatabase/SampleDatabase/Model/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleDatabase.Model
+{
+    public class Author
+    {
+        // ＊　主キーの指定　＊
+        // Entity Frameworkでは"Id"という名前のプロパティ、
+        // またはクラス名と"Id"を組み合わせた名前のプロパティを主キーとして扱う
+
+        // 主キーとはデータベースの中からそのオブジェクトを一意に識別できる項目のこと
+        // このId列にはIDENTITY属性が付与され、行が追加されるごとに列の値が自動採番される。
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+
+        // AuthorクラスにはICollection<Book>プロパティを作成する
+        // 著者からその著者が書いた書籍情報にアクセスできるようにする
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
