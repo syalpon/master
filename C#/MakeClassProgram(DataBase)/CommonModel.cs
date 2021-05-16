fe13ea4c68489fe99a976f1e8333d5968b01c04d
hosomi.takuya <accelerator916@gmail.com>
2021-05-16 23:20:12 +0900

    しらねーよ！！！

---
+++ C#/MakeClassProgram(DataBase)/CommonModel.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// MVCモデルにおけるM:モデル
+/// </summary>
+class CommonModel
+{
+    protected View _view;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public CommonModel()
+    {
+        _view = new View();
+    }
+
+    /// <summary>
+    /// ジェネリックで各Enumの文字列を取得する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    protected string[] GetSelection<T>()
+    {
+        string[] choices = new string[Enum.GetNames(typeof(T)).Length];
+        foreach (int Value in Enum.GetValues(typeof(T)))
+        {
+            string name = Enum.GetName(typeof(T), Value);
+            choices[Value - 1] = name;
+        }
+        return choices;
+    }
+
+    /// <summary>
+    /// 「アクセス修飾子を入力してください。」を返す
+    /// </summary>
+    /// <returns></returns>
+    public string GetInputAccessor()
+    {
+        return "アクセス修飾子を入力してください。\n";
+    }
+
+    /// <summary>
+    /// 「型名を選択してください。」を返す
+    /// </summary>
+    /// <returns></returns>
+    public string GetInputType()
+    {
+        return "型名を選択してください。\n";
+    }
+}
